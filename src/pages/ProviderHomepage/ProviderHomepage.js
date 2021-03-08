cedb936a0eb3d0df82eed2b735055ce7c659c603
Georgia Adams <georgia.adams92@gmail.com>
2021-03-08 11:29:07 +0100

    upadted

--- src/pages/ProviderHomepage/ProviderHomepage.js
+++ src/pages/ProviderHomepage/ProviderHomepage.js
@@ -48,39 +48,4 @@ class ProviderHomepage extends React.Component {
   }
 }
 
-// const ProviderHomepage = () => {
-//   const [requestedOffers, setRequestedOffers] = React.useState([]);
-
-//   React.useEffect(() => {
-//     async function getOffers() {
-//       const offers = await providerService.getAllOffersProvider();
-//       const data = offers.filter((offer) => offer.status === "requested");
-//       setRequestedOffers(data);
-//     }
-//     getOffers();
-//   }, []);
-
-//   console.log("requested", requestedOffers);
-// return (
-//     <div className="provider-home">
-//       <img
-//         className="care-icon"
-//         src="https://res.cloudinary.com/skillbees/image/upload/v1615127951/Meal.ly/love_louyw3.png"
-//         alt="care-icon"
-//       />
-//       <Link to={"/provider/offers/new"}>
-//         <button className="add-new-offer">Add a new offer</button>
-//       </Link>
-//       <h3>Your requested offers:</h3>
-//       {requestedOffers.map((offer) => (
-//         <p>{offer.comments}</p>
-//       ))}
-//       <Link to={"/provider/offers"}>
-//         <button className="offers-panel-button">All my offers</button>
-//       </Link>
-//       <BottomNavbar />
-//     </div>
-//   );
-// };
-
 export default ProviderHomepage;
